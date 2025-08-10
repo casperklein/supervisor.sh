b9b23d7a9288f4963c030651e6854bed5b1adcc3
casperklein <casperklein@users.noreply.github.com>
2025-08-10 23:40:45 +0200

    version 0.5

--- supervisor.sh
+++ supervisor.sh
@@ -12,7 +12,7 @@ shopt -s nullglob        # Return nothing if '*' does not expand
 # Global variables
 APP="supervisor.sh"
 APP_PATH=$(readlink -f "$0")
-VER=0.4
+VER=0.5
 
 PID_DIR="/run/$APP"
 PID_FILE="$PID_DIR/$APP.pid"
